03716a187e99e28864ddbd0e3143c2fb56d89a64
Madura Eeswara <madura.madushan@outlook.com>
2019-05-14 17:50:27 +0530

    user login

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongosse = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongosse.Schema({
     name:{
@@ -46,6 +47,26 @@ userSchema.methods.toJSON = function(){
     delete userObject.tokens
 
     return userObject
+}
+
+userSchema.methods.generateAuthToken = async function(){
+    const user = this
+    const token = jwt.sign({_id: user._id.toString()}, 'max_games')
+
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email})
+    if(!user) throw new Error('Unable to login')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) throw new Error('Unable to login')
+
+    return user
 }
 
 userSchema.pre('save', async function (next){
--- routes/user.js
+++ routes/user.js
@@ -12,4 +12,14 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
+    }catch(e){
+        res.status(400).send()
+    }
+})
+
 module.exports = router
