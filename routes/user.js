ff4407096a607cd1103b12f5b51eabf494edc8f4
Madura Eeswara <madura.madushan@outlook.com>
2019-05-14 18:06:57 +0530

    logout user

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('./../models/user')
+const auth = require('./../middleware/auth')
 
 router.post('/', async(req, res) => {
     const user = new User(req.body)
@@ -19,6 +20,16 @@ router.post('/login', async (req, res) => {
         res.send({user, token})
     }catch(e){
         res.status(400).send()
+    }
+})
+
+router.post('/logout', auth, async (req, res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
+        await req.user.save()
+        res.send()
+    }catch(e){
+        res.status(500).send()
     }
 })
 
